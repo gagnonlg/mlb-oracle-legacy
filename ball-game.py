5ca3a87bf4b22c6b7586b0c2ea9aec2d605f7024
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2020-07-29 13:16:13 -0400

    Add interactive mode

--- ball-game.py
+++ ball-game.py
@@ -8,19 +8,91 @@
 
 # Batter: AB H 2B 3B HR SO BA
 
+import collections
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 class ScoreBoard(object):
     def __init__(self):
-        self.home, self.away = 0, 0
+        self.inning = 1
+
+        self.home, self.away = 0, 0  # total score
+        self.inning_home = collections.defaultdict(int)
+        self.inning_away = collections.defaultdict(int)
 
     def tied(self):
         return self.home == self.away
 
+    def add_runs_away(self, runs):
+        self.away += runs
+        self.inning_away[self.inning] += runs
+
+    def add_runs_home(self, runs):
+        self.home += runs
+        self.inning_home[self.inning] += runs
+
     def update(self, runs_home, runs_away):
         self.home += runs_home
         self.away += runs_away
+        self.inning_home[self.inning] += runs_home
+        self.inning_away[self.inning] += runs_away
+
+    def next_inning(self):
+        self.inning += 1
+
+    def post(self):
+        vspace(2)
+        n = max(9, self.inning)
+        header = ' I | ' + ' | '.join(['{}'.format(i) for i in range(1, n + 1)]) + ' | R'
+        print(header)
+        print('-' * len(header))
+        print(
+            ' A | ' + '   '.join([('{}'.format(self.inning_away.get(i, ' ')))for i in range(1, n + 1)]) + ' | {}'.format(
+                self.away
+            )
+        )
+        print('-' * len(header))
+        print(
+            ' H | ' + '   '.join([('{}'.format(self.inning_home.get(i, ' ')))for i in range(1, n + 1)]) + ' | {}'.format(
+                self.home
+            )
+        )
+
+def welcome_message():
+    print('*** welcome to ball-game.py v.alpha-0 ***')
+
+
+def vspace(nspace=5):
+    while nspace > 0:
+        print()
+        nspace -= 1
+
+def play_game_interactively(home, away):
+
+    score = ScoreBoard()
+
+    welcome_message()
+    score.post()
+
+    for _ in range(8):
+       runs_away = play_half_inning_interactively(offense=away, defense=home)
+       score.add_runs_away(runs_away)
+       score.post()
+       runs_home = play_half_inning_interactively(offense=home, defense=away)
+       score.add_runs_home(runs_home)
+       score.post()
+       score.next_inning()
+
+    runs_away = play_half_inning(offense=away, defense=home)
+    score.add_runs_away(runs_away)
+    score.post()
+
+    if score.home <= score.away:
+        runs_home = play_half_inning(offense=home, defense=away)
+        score.add_runs_home(runs_home)
+        score.post()
+
 
 def play_game(home, away):
 
@@ -103,6 +175,33 @@ class Field(object):
         self.bases[i] = 0
 
 
+def play_half_inning_interactively(offense, defense, overtime=False):
+    field = Field(overtime)
+    outs = 0
+    while outs < 3:
+        pitcher = defense.current_pitcher()
+        batter = offense.next_batter()
+        print('*** Pitcher: ' + pitcher['name'])
+        print('+++ Batter: ' + batter['name'])
+        print('Press any key to pitch!')
+        input()
+        nouts, outcome = simulate_at_bat(
+            field,
+            pitcher,
+            batter
+        )
+        outs += nouts
+        print('{}!'.format(outcome))
+        print('Outs: {}'.format(outs))
+        print('Bases: {}'.format(' '.join([
+            '1B' if field.bases[0] == 1 else '__',
+            '2B' if field.bases[1] == 1 else '__',
+            '3B' if field.bases[2] == 1 else '__',
+        ])))
+        print('Runs: {}'.format(field.run_counter))
+    return field.run_counter
+
+
 def play_half_inning(offense, defense, overtime=False):
     field = Field(overtime)
     outs = 0
@@ -142,16 +241,16 @@ def simulate_at_bat(field, pitcher, batter):
         field.advance(1)
         field.out()
 
-    return int(outcome in ['TagOut', 'Flyout', 'Strikeout'])
+    return int(outcome in ['TagOut', 'Flyout', 'Strikeout']), outcome
 
 
 # Pitcher: H BB SO BF
-def pitcher(H, BB, SO, BF):
-    return {'H': H, 'BB': BB, 'SO': SO, 'BF': BF}
+def pitcher(name, H, BB, SO, BF):
+    return {'name': name, 'H': H, 'BB': BB, 'SO': SO, 'BF': BF}
 
 # Batter: AB H 2B 3B HR SO BA
-def batter(AB, H, TWOB, THREEB, HR, SO, BA):
-    return {'AB': AB, 'H': H, '2B': TWOB, '3B': THREEB, 'HR': HR, 'SO': SO, 'BA': BA}
+def batter(name, AB, H, TWOB, THREEB, HR, SO, BA):
+    return {'name': name, 'AB': AB, 'H': H, '2B': TWOB, '3B': THREEB, 'HR': HR, 'SO': SO, 'BA': BA}
 
 
 def compute_prob_dist(pitcher, batter):
@@ -186,63 +285,66 @@ def compute_prob_dist(pitcher, batter):
 
 ATHLETICS = Team(
     # Pitcher: H BB SO BF
-    # pitcher=pitcher(459, 131, 402, 2049), # Sean Manaea
-    pitcher=pitcher(214, 76, 207, 913), # Frankie Montas
+    # pitcher=pitcher("Sean Manaea", 459, 131, 402, 2049),
+    pitcher=pitcher("Frankie Montas", 214, 76, 207, 913),
     batters=[
         # Batter: AB H 2B 3B HR SO BA
-        batter(3059, 783, 161, 21, 108, 682, 0.256), # Marcus Semien,
-        batter(593, 172, 42, 1, 30, 174, 0.290), # Ramón Laureano,
-        batter(1425, 366, 101, 12, 74, 386, 0.257), # Matt Chapman,
-        batter(1277, 325, 62, 0, 90, 367, 0.255), # Matt Olson,
-        batter(1434, 357, 73, 7, 67, 369, 0.249), # Mark Canha,
-        batter(3210, 782, 154, 8, 216, 961, 0.244), # Khris Davis
-        batter(2197, 556, 119, 8, 42, 536, 0.253), # Robbie Grossman
-        batter(975, 239, 52, 2, 42, 282, 0.245), # Chad Pinder,
-        batter(66, 14, 4, 0, 0, 22, 0.212), #  Austin Allen
-        # batter(54, 13, 5, 0, 4, 17, 0.241), # Sean Murphy,
-        # batter(2062, 545, 124, 9, 78, 474, 0.264), # Stephen Piscotty,
+        batter("Marcus Semien", 3059, 783, 161, 21, 108, 682, 0.256),
+        batter("Ramón Laureano", 593, 172, 42, 1, 30, 174, 0.290),
+        batter("Matt Chapman", 1425, 366, 101, 12, 74, 386, 0.257),
+        batter("Matt Olson", 1277, 325, 62, 0, 90, 367, 0.255),
+        batter("Mark Canha", 1434, 357, 73, 7, 67, 369, 0.249),
+        batter("Khris Davis", 3210, 782, 154, 8, 216, 961, 0.244),
+        batter("Robbie Grossman", 2197, 556, 119, 8, 42, 536, 0.253),
+        batter("Chad Pinder", 975, 239, 52, 2, 42, 282, 0.245),
+        batter(" Austin Allen", 66, 14, 4, 0, 0, 22, 0.212),
+        # batter("Sean Murphy", 54, 13, 5, 0, 4, 17, 0.241),
+        # batter("Stephen Piscotty", 2062, 545, 124, 9, 78, 474, 0.264),
     ]
 )
 
 ANGELS = Team(
     # Pitcher: H BB SO BF
-    pitcher=pitcher(611, 206, 602, 2628), # Dylan Bundy
+    pitcher=pitcher("Dylan Bundy", 611, 206, 602, 2628),
     batters=[
         # Batter: AB H 2B 3B HR SO BA
-        batter(884, 252, 48, 6, 7, 100, 0.285), # David Fletcher
-        batter(4343, 1325, 251, 46, 285, 1119, 0.305), # Mike Trout
-        batter(6212, 1651, 332, 38, 298, 1799, 0.266), # Justin Upton
-        batter(10691, 3202, 661, 16, 356, 1280, 0.3), # Albert Pujols
-        batter(177, 32, 6, 0, 7, 68, 0.181), # Taylor Ward
-        batter(1124, 306, 58, 2, 26, 147, 0.272), # Tommy La Stella
-        batter(93, 17, 5, 1, 1, 36, 0.183), # Michael Hermosillo
-        batter(432, 88, 17, 0, 12, 145, 0.204), # Max Stassi ,
-        batter(3841, 1030, 181, 23, 67, 370, 0.268), # Andrelton Simmons
-        # batter(2677, 620, 148, 9, 87, 844, 0.232), #  Jason Castro
-        # batter(868, 222, 60, 5, 36, 270, 0.256), # Brian Goodwin
-        # batter(715, 204, 41, 7, 40, 213, 0.285), # Shohei Ohtani
+        batter("David Fletcher", 884, 252, 48, 6, 7, 100, 0.285),
+        batter("Mike Trout", 4343, 1325, 251, 46, 285, 1119, 0.305),
+        batter("Justin Upton", 6212, 1651, 332, 38, 298, 1799, 0.266),
+        batter("Albert Pujols", 10691, 3202, 661, 16, 356, 1280, 0.3),
+        batter("Taylor Ward", 177, 32, 6, 0, 7, 68, 0.181),
+        batter("Tommy La Stella", 1124, 306, 58, 2, 26, 147, 0.272),
+        batter("Michael Hermosillo", 93, 17, 5, 1, 1, 36, 0.183),
+        batter("Max Stassi ,", 432, 88, 17, 0, 12, 145, 0.204),
+        batter("Andrelton Simmons", 3841, 1030, 181, 23, 67, 370, 0.268),
+        # batter("Jason Castro", 2677, 620, 148, 9, 87, 844, 0.232),
+        # batter("Brian Goodwin", 868, 222, 60, 5, 36, 270, 0.256),
+        # batter("Shohei Ohtani", 715, 204, 41, 7, 40, 213, 0.285),
     ]
 )
 
 ROCKIES = Team(
     # Pitcher: H BB SO BF
-    pitcher=pitcher(557, 150, 573, 2360),  # German Márquez
+    pitcher=pitcher("German Márquez", 557, 150, 573, 2360),
     batters=[
         # Batter: AB H 2B 3B HR SO BA
-        batter(862, 256, 53, 12, 38, 239, 0.297),  # David Dahl
-        batter(2075, 572, 133, 18, 125, 667, 0.276),  # Trevor Story
-        batter(4108, 1247, 227, 47, 172, 760, 0.304),  # Charlie Blackmon
-        batter(3949, 1163, 253, 27,227, 665, 0.295),  # Nolan Arenado
-        batter(5197, 1546, 369, 29, 135, 693, 0.297),  # Daniel Murphy
-        batter(694, 167, 33, 2, 29, 237, 0.241),  # Ryan McMahon
-        batter(660, 180, 37, 8, 12, 159, 0.273),  # Raimel Tapia
-        batter(88, 23, 5, 2, 8, 31, 0.261),  # Sam Hilliard
-        batter(986, 235, 45, 9, 7, 210, 0.238),  # Tony Wolters
+        batter("David Dahl", 862, 256, 53, 12, 38, 239, 0.297),
+        batter("Trevor Story", 2075, 572, 133, 18, 125, 667, 0.276),
+        batter("Charlie Blackmon", 4108, 1247, 227, 47, 172, 760, 0.304),
+        batter("Nolan Arenado", 3949, 1163, 253, 27,227, 665, 0.295),
+        batter("Daniel Murphy", 5197, 1546, 369, 29, 135, 693, 0.297),
+        batter("Ryan McMahon", 694, 167, 33, 2, 29, 237, 0.241),
+        batter("Raimel Tapia", 660, 180, 37, 8, 12, 159, 0.273),
+        batter("Sam Hilliard", 88, 23, 5, 2, 8, 31, 0.261),
+        batter("Tony Wolters", 986, 235, 45, 9, 7, 210, 0.238),
     ]
 )
 
 HOME = ATHLETICS
 AWAY = ROCKIES
+
+play_game_interactively(ATHLETICS, ROCKIES)
+exit()
 
 # score = play_game(ATHLETICS, ANGELS)
 # print(score.home, score.away)
