941be10697960676c553de319c04efd23d8c9eb4
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2020-07-31 10:20:52 -0400

    mlb-oracle

--- lineups.py
+++ lineups.py
@@ -14,7 +14,7 @@ import ballgame
 # d['awayBatters'][2]
 
 
-sched = statsapi.schedule(start_date='07/31/2020')
+sched = statsapi.schedule(start_date='07/30/2020')
 # print (sched[0])
 
 # dict_keys(['gamesPlayed', 'gamesStarted', 'groundOuts', 'airOuts', 'runs', 'doubles', 'triples', 'homeRuns', 'strikeOuts', 'baseOnBalls', 'intentionalWalks', 'hits', 'hitByPitch', 'avg', 'atBats', 'obp', 'slg', 'ops', 'caughtStealing', 'stolenBases', 'stolenBasePercentage', 'groundIntoDoublePlay', 'numberOfPitches', 'era', 'inningsPitched', 'wins', 'losses', 'saves', 'saveOpportunities', 'holds', 'earnedRuns', 'whip', 'battersFaced', 'gamesPitched', 'completeGames', 'shutouts', 'strikes', 'strikePercentage', 'hitBatsmen', 'balks', 'wildPitches', 'pickoffs', 'groundOutsToAirouts', 'winPercentage', 'pitchesPerInning', 'gamesFinished', 'strikeoutWalkRatio', 'strikeoutsPer9Inn', 'walksPer9Inn', 'hitsPer9Inn', 'runsScoredPer9', 'homeRunsPer9', 'inheritedRunners', 'inheritedRunnersScored', 'sacBunts', 'sacFlies'])
@@ -142,27 +142,3 @@ if __name__ == '__main__':
     for game in sched:
         run_game(game)
 
-# for game in sched:
-#     game_data = statsapi.boxscore_data(game['game_id'])
-#     print('==> {} {} vs {}'.format(
-#         dateutil.parser.isoparse(game['game_datetime']),
-#         game['away_name'],
-#         game['home_name'],
-#     ))
-
-#     try:
-#         team_away = lineup(game, game_data, 'away')
-#         team_home = lineup(game, game_data, 'home')
-#     except KeyError:
-#         print('** WARNING: problem setting up the simulation, skipping')
-#         continue
-
-#     if not team_away or not team_home:
-#         continue
-
-#     print('  -> Computing expected outcome')
-#     hwp = estimate_home_win_probability(team_away, team_home)
-
-
-#     # pred = ballgame.most_probable_outcome(team_away, team_home)
-#     # print('{}: {}\n{}: {}'.format(game['away_name'], pred[0], game['home_name'], pred[1]))
