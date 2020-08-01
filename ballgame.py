6c3543668cc637d99fb7221739d9399c8e16ef63
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2020-08-01 16:34:33 -0400

    add some notes

--- ballgame.py
+++ ballgame.py
@@ -1,18 +1,48 @@
+"""Towards 1.0
+
+* Setup a package so that the library is transparantly available
+
+* Architecturally, 
+
+
+* From the c++-side, return 2D array with the unormalized joint
+  distribution over runs then do all postprocessing on python side.
+* Make a pair of Team objects the data bridge between python and c++
+  * Team objects are something like a collection of Player objects
+  * Thus the api is Team, Player, (double**) run_simulation().
+  * The maximum score is a built-in constant that can be queried to get
+    the size of the result matrix. This matrix does not have underflow
+    or overflow bins; invalid inputs are discarded. The idea is that
+    we can choose a size that's very small like 256, i.e. it requires
+    barely any memory but the odds of score reaching that high are
+    practically zero.
+  * Matrix.at(i,j) => P(A = i, H = j)
+
+* Relegate threading to python side. Since the simulation must return
+  the unormalized distributions over run, the results are trivialy
+  consolidated in numpy code. This also means that the simulator entry
+  point could be a python function altogether (e.g. keras model), and
+  be transparantly multi-threaded (modulo GIL).
+
+
+* Have Player objects be references to a stats database which can be
+  queried.
+* Which allows validation studies!
+
+"""
+
+
 import ctypes
 
 libballgame = ctypes.cdll.LoadLibrary('./libballgame.so')
 
 
-# TODO have this as a full-fledged c++ object and use it as bridge
-# instead of via files
 class Team(object):
     def __init__(self, pitcher, batters):
         self.pitcher = pitcher
         self.batters = batters
 
 
-# TODO batter and pitcher also should be c++-side objects. They should
-# ideally be a player class that's an id into a local stats database
 def batter(name, AB, H, TWOB, THREEB, HR, SO, BA):
     return {'name': name, 'AB': AB, 'H': H, '2B': TWOB, '3B': THREEB, 'HR': HR, 'SO': SO, 'BA': BA}
 
